486593dfb3fdf5c82a15c498b232bc7fe4d3f57e
SimkaBoi <simkaviiner@gmail.com>
2021-03-22 12:11:49 +0200

    add canvas

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <canvas ref="canvas" width="100vw" height="100vh"> </canvas>
+</template>
+
+<script>
+export default {
+  id: "App",
+  mounted() {},
+};
+</script>
+
+<style></style>
